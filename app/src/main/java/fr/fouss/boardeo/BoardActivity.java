ebf8f7842d96a177d8430f4a45adfdf501b237a1
Ezia <esia.belbachir@protonmail.com>
2017-10-31 21:51:58 -0400

    Start dev of board activity

---
+++ app/src/main/java/fr/fouss/boardeo/BoardActivity.java
@@ -0,0 +1,20 @@
+package fr.fouss.boardeo;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+public class BoardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+
+//        RecyclerView myRecyclerView = findViewById(R.id.boardList);
+//        BoardItemAdapter adapter = new BoardItemAdapter();
+//        myRecyclerView.setAdapter(adapter);
+    }
+}
