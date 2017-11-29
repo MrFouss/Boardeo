9d71abc217255ad9eddbd1df0af7bad112daae3a
MrFouss <maxime.brodat@fouss.fr>
2017-11-29 18:50:39 -0500

    Add Post Adapter

--- app/src/main/java/fr/fouss/boardeo/data/Post.java
+++ app/src/main/java/fr/fouss/boardeo/data/Post.java
@@ -7,35 +7,40 @@ import java.util.Map;
 
 public class Post {
 
-    public final static String BOARD_KEY_FIELD = "postBoardKey";
-    public final static String AUTHOR_UID_FIELD = "postAuthorUid";
+    public final static String TITLE_FIELD = "postTitle";
     public final static String CONTENT_FIELD = "postContent";
     public final static String TIMESTAMP_FIELD = "postTimestamp";
+    public final static String AUTHOR_UID_FIELD = "postAuthorUid";
+    public final static String BOARD_KEY_FIELD = "postBoardKey";
 
-    private String boardKey;
-    private String authorUid;
+    private String title;
     private String content;
     private Long timestamp;
+    private String authorUid;
+    private String boardKey;
 
     public Post() {}
 
-    public Post(String boardKey,
-            String authorUid,
-            String content,
-            Long timestamp) {
+    public Post(String title,
+                String content,
+                Long timestamp,
+                String authorUid,
+                String boardKey) {
 
-        this.boardKey = boardKey;
-        this.authorUid = authorUid;
+        this.title = title;
         this.content = content;
         this.timestamp = timestamp;
+        this.authorUid = authorUid;
+        this.boardKey = boardKey;
     }
 
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
-        result.put(BOARD_KEY_FIELD, getBoardKey());
-        result.put(AUTHOR_UID_FIELD, getAuthorUid());
+        result.put(TITLE_FIELD, getTitle());
         result.put(CONTENT_FIELD, getContent());
+        result.put(AUTHOR_UID_FIELD, getAuthorUid());
         result.put(TIMESTAMP_FIELD, getTimestamp());
+        result.put(BOARD_KEY_FIELD, getBoardKey());
 
         return result;
     }
@@ -46,10 +51,11 @@ public class Post {
      * @param intent the intent containing the comment's info
      */
     public void setFromIntent(Intent intent) {
-        this.boardKey = intent.getStringExtra(BOARD_KEY_FIELD);
-        this.authorUid = intent.getStringExtra(AUTHOR_UID_FIELD);
+        this.title = intent.getStringExtra(TITLE_FIELD);
         this.content = intent.getStringExtra(CONTENT_FIELD);
         this.timestamp = intent.getLongExtra(TIMESTAMP_FIELD, 0);
+        this.authorUid = intent.getStringExtra(AUTHOR_UID_FIELD);
+        this.boardKey = intent.getStringExtra(BOARD_KEY_FIELD);
     }
 
     /**
@@ -59,20 +65,17 @@ public class Post {
      * @return the same intent, but filled with the board's info
      */
     public Intent fillIntentExtras(Intent intent) {
-        intent.putExtra(BOARD_KEY_FIELD, getBoardKey());
-        intent.putExtra(AUTHOR_UID_FIELD, getAuthorUid());
+        intent.putExtra(TITLE_FIELD, getTitle());
         intent.putExtra(CONTENT_FIELD, getContent());
         intent.putExtra(TIMESTAMP_FIELD, getTimestamp());
+        intent.putExtra(AUTHOR_UID_FIELD, getAuthorUid());
+        intent.putExtra(BOARD_KEY_FIELD, getBoardKey());
 
         return intent;
     }
 
-    public String getBoardKey() {
-        return boardKey;
-    }
-
-    public String getAuthorUid() {
-        return authorUid;
+    public String getTitle() {
+        return title;
     }
 
     public String getContent() {
@@ -83,12 +86,16 @@ public class Post {
         return timestamp;
     }
 
-    public void setBoardKey(String boardKey) {
-        this.boardKey = boardKey;
+    public String getAuthorUid() {
+        return authorUid;
     }
 
-    public void setAuthorUid(String authorUid) {
-        this.authorUid = authorUid;
+    public String getBoardKey() {
+        return boardKey;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public void setContent(String content) {
@@ -97,5 +104,13 @@ public class Post {
 
     public void setTimestamp(Long timestamp) {
         this.timestamp = timestamp;
+    }
+
+    public void setAuthorUid(String authorUid) {
+        this.authorUid = authorUid;
+    }
+
+    public void setBoardKey(String boardKey) {
+        this.boardKey = boardKey;
     }
 }
